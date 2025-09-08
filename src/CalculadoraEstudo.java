8e78fa31e6c4d8d5fba0e9de743edd9239b824a3
ceciliascm <ceciliascm@gmail.com>
2025-09-08 10:27:33 -0300

    first commit: Desafio 1 -> CalculadoraEstudo

---
+++ src/CalculadoraEstudo.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class CalculadoraEstudo {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Ler quantidade de dias
+        System.out.print("Quantos dias de estudo por semana(no máximo 7)? ");
+        int dias = scanner.nextInt();
+
+        if(dias <= 0 || dias > 7){
+            System.out.println("Dados inválidos! Os dias precisam estar de 1 a 7");
+        } else {
+            // Vetor para armazenar as horas
+            int[] horas = new int[dias];
+            int total = 0;
+
+            // Ler horas por dia
+            for (int i = 0; i < dias; i++) {
+                System.out.print("Horas de estudo no dia " + (i + 1) + ": ");
+                horas[i] = scanner.nextInt();
+                total += horas[i];
+            }
+
+            // Calcular média
+            double media = (double) total / dias;
+
+            // Mostrar resultados
+            System.out.println("\nDias de estudo: " + dias);
+            System.out.print("Horas: ");
+            for (int h : horas) {
+                System.out.print(h + " ");
+            }
+            System.out.println("\nTotal: " + total + " horas");
+            System.out.printf("Média: %.1f horas/dia \n", media);
+
+            // Verificar meta
+            if (total >= 20) {
+                System.out.println("Meta atingida!");
+            } else {
+                System.out.println("Meta não atingida.");
+            }
+
+            scanner.close();
+        }
+    }
+}
